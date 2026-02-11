0064d97c7975610670b8d242b4dea0f5bc7b48d5
Ayush Devadiga <ayushdevadiga2005@gmail.com>
2026-02-11 14:19:07 +0530

    LAMBDA FUNCTIONS 2

--- lambda_functions.py
+++ lambda_functions.py
@@ -27,3 +27,30 @@ new_title = "".join(map(lambda x : x.capitalize(),title_words))
 print(new_title)
 
 print(f'\n{f'-'*188}\n')
+
+# Like map filter also takes the list as well as the func as parameter
+numbers = [1,2,3,4,4,5,6,67,7,7,8,9,3]
+even_numbers = list(filter(lambda x : x%2 ==0 , numbers)) # Note here we can also use other dtypes other than list
+odd_numbers = [ x for x in numbers if x not in even_numbers ]
+print(f'The numbers list : {numbers} \n Odd numbers  : {odd_numbers} \n Even numbers : {even_numbers}')
+
+
+# Now lets also see how we can use this ins sorting
+students = [    
+                ('Austin',17) , ('Chris',18) , ('Jeff',17)
+]
+print(f'The actual list : {students}')
+
+sorted_students_by_name = sorted(students,key = lambda x: x[0]) # Sorted takes two params : The iterable and the key = logical function
+sorted_students_by_age = sorted(students,key = lambda x: x[1])
+
+print(f'Sorted the students by name \n {sorted_students_by_name}')
+print(f'Sorted the students by age \n {sorted_students_by_age}')
+
+print(f'\n{f'-'*188}\n')
+
+# Now lets see another function
+
+fruits = ['Orange','Kiwi','Mango','Apple']
+sorted_by_length = sorted(fruits,key=lambda x : len(x))
+print(f'Sorted the fruits by length \n {sorted_by_length}')
