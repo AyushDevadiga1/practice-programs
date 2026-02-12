5c3dd039c1cec8b1c33b3e17007d31ed9b322b12
Ayush Devadiga <ayushdevadiga2005@gmail.com>
2026-02-12 21:19:45 +0530

    File Handling 3

--- file_handling.py
+++ file_handling.py
@@ -1,6 +1,6 @@
-'''
-
 file_path = 'dummy_text.txt'
+
+'''
 
 file_opened = open(file_path,'r')
 
@@ -11,7 +11,7 @@ print(f'_'*149)
 # Alternatively we can use the with statement which can be used for better handling 
 # as the reader/scanner is automatically closed here
 
-file_path = 'dummy_text.txt'
+
 
 with open(file_path,'r') as f:
     data = f.read()
@@ -24,7 +24,7 @@ print(f'_'*149)
 
 '''
 
-file_path = 'dummy_text.txt'
+
 
 file_opened = open(file_path,'r')
 
@@ -35,7 +35,7 @@ file_opened.close() # Good Practice is to close close the Reader
 
 print(f'_'*149)
 
-file_path = 'dummy_text.txt'
+
 
 with open(file_path,'r') as f:
     print(f.readline()) # Reads a Single line if para passed 
@@ -44,7 +44,7 @@ file_opened.close() # Good Practice is to close close the Reader
 
 print(f'_'*149)
 
-file_path = 'dummy_text.txt'
+
 
 with open(file_path,'r') as f:
     for x in f : # <-- This helps to loop each line
@@ -55,3 +55,58 @@ file_opened.close() # Good Practice is to close close the Reader
 
 '''
 
+'''
+
+To create a new file in Python, use the open() method, with one of the following parameters:
+
+"x" - Create - will create a file, returns an error if the file exists
+
+"a" - Append - will create a file if the specified file does not exists
+
+"w" - Write - will create a file if the specified file does not exists
+
+'''
+
+'''
+
+new_file = open('new_demo_file','x') 
+
+print(f'_'*149)
+
+print('Before Appending')
+
+print(f'_'*149)
+
+with open(file_path,'r') as f:
+    print(f.read())
+
+print(f'_'*149)
+
+with open(file_path,'a') as f:
+    f.write(' Something\'s in the way ... ')
+
+print(f'_'*149)
+
+print('After Appending')
+
+print(f'_'*149)
+
+with open(file_path,'r') as f:
+    print(f.read())
+
+print(f'_'*149)
+
+'''
+
+'''
+# Now deleting files
+
+import os
+
+path_for_deletion = 'new_demo_file.txt'
+
+if path_for_deletion:
+    os.remove(path_for_deletion)
+else:
+    print('The path does not exists !!! ')
+'''
